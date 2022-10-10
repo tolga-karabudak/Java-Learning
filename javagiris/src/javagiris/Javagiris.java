f3433463e27096ea42d4118aff953e48377eb92d
Tolgahan Karabudak <tolgakarabudak2@gmail.com>
2022-10-10 19:34:52 +0300

    Giriş ve Hello Word

---
+++ javagiris/src/javagiris/Javagiris.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javagiris;
+
+/**
+ *
+ * @author tolga
+ */
+public class Javagiris {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int yas;
+        yas = 25;
+        System.out.println("hello"); // ilk java kodum
+        System.out.println("Tolgahan Karabudak \n yas : " + yas);
+        System.out.println("New line komutunu print edebilmem icin iki yane backslash kullanmam gerekti \\n");
+        String s = "benim adim tolga";
+        double pi = 3.14;
+        System.out.println("String basiyorum : " + s);
+        System.out.println("Double pi sayisi basiyor : " + pi);
+    }
+    /* temel bilgi yüklemesi
+    yapıyorum
+    Tolgahan
+    Karbaudak
+    
+    */
+}
